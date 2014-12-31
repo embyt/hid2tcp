4a256644f642bd191aca300cb1aa9f88b854cb7e
roman <maemo@morawek.at>
2014-12-31 16:01:41 +0100

    - adapt to client/server architecture

--- hid2tcp.py
+++ hid2tcp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import logging
 import threading
-import time
 import os
+import http.server
+import base64
+
 
 #os.environ['PYUSB_DEBUG_LEVEL'] = 'debug'
 #os.environ['PYUSB_DEBUG'] = 'debug'
@@ -12,15 +14,16 @@ import usb.util
 
 VENDOR_ID  = 0x188a
 PRODUCT_ID = 0x1101
-
+HTTP_PORT  = 1711
 
 def receiver(dev, endpoint):
+    logging.getLogger().info("Reading...")
     while True:
-        logging.getLogger().info("Reading...")
         try:
             bytes = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize, timeout=10000)
         except Exception as e:
-            logging.getLogger().info("Could not read data: {}".format(e))
+            # most probably this is a read timeout
+            logging.getLogger().debug("Could not read data: {}".format(e))
             continue
 
         # got data
@@ -28,9 +31,54 @@ def receiver(dev, endpoint):
                 ''.join(['%02x ' % abyte for abyte in bytes])))
 
 
+class HttpRequestHandler(http.server.BaseHTTPRequestHandler):
+    def parse_url(self):
+        """checks for valid path, which must be: /VENDOR_ID/PRODUCT_ID/RF_PACKET"""
+        request_path = self.path.split("/")
+        
+        # check for number of arguments
+        if len(request_path) != 4:
+            logging.getLogger().warning('Illegal URL requested: '.format(self.path))
+            self.send_error(404, 'Illegal URL requested: '.format(self.path))
+            return False
+        # check for valid vendor/product ID
+        if request_path[1][1:] != str(VENDOR_ID) or request_path[2][1:] != str(PRODUCT_ID):
+            logging.getLogger().warning('Illegal vendor/product ID requested: '.format(self.path))
+            self.send_error(404, 'Illegal vendor/product ID requested: '.format(self.path))
+            return False
+            
+        # decode RF data
+        send_packet_base64 = (request_path[3]+"==")
+        self.send_packet = base64.b64decode(send_packet_base64.encode('ascii'), b'-_')
+        logging.getLogger().info("RF packet to send: {}".format(
+                ''.join(['%02x ' % abyte for abyte in self.send_packet])))
+        return True
+
+    def do_GET(self):
+        if self.parse_url():
+            # send packet
+            self.server.usb_endpoint_out.write(self.send_packet)
+
+            # get RF response
+
+            # set http response
+            self.send_response(200)
+            self.send_header('Content-type', 'text/uuencode')
+            self.end_headers()
+            self.wfile.write("Test Responsepacket.".encode())
+
+    def do_POST(self):
+        if self.parse_url():
+            # send packet
+            self.server.usb_endpoint_out.write(self.send_packet)
+
+            # set http response
+            self.send_response(200)
+            self.send_header('Content-type', 'text/ascii')
+            self.end_headers()
+
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    
     # find device
     dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
     if dev is None:
@@ -59,20 +107,19 @@ def main():
     # start receiver thread
     treceiver = threading.Thread(target=receiver, args=(dev, endpoint_in))
     treceiver.start()
-       
-    while True:
-        # perform communication
-        time.sleep(3)
-        packet = bytes([4,0xb2,0x1b,0])
-        logging.getLogger().info("Sending {}.".format(
-                ''.join(['%02x ' % int(abyte) for abyte in packet])))
-        endpoint_out.write(packet)
-
+    
+    # start webserver
+    server_address = ('', HTTP_PORT)
+    httpd = http.server.HTTPServer(server_address, HttpRequestHandler)
+    httpd.usb_endpoint_out = endpoint_out
+    httpd.serve_forever()
+    
     # done
     logging.getLogger().info("release claimed interface")
     #usb.util.release_interface(dev, interface)
 
 
 if __name__ == '__main__':
-  main()
+    logging.basicConfig(level=logging.INFO)
+    main()
 
---
+++ test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import logging
+import urllib.request
+import base64
+
+
+VENDOR_ID  = 0x188a
+PRODUCT_ID = 0x1101
+HTTP_PORT  = 1711
+
+packet = bytes([4, 0xb2, 0x1b, 0])
+
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger().info("Sending {}.".format(
+        ''.join(['%02x ' % int(abyte) for abyte in packet])))
+
+# create URL
+packet_base64=base64.b64encode(packet, b'-_').decode('ascii')[:-2]
+url = "http://localhost:{}/v{}/p{}/{}".format(HTTP_PORT, VENDOR_ID, PRODUCT_ID, packet_base64)
+logging.getLogger().info("Requesting {}.".format(url))
+
+# send request
+response = urllib.request.urlopen(url)
+
+# dump answer
+print(response.read())
+
